d9f407db68f7e3b5e8f9abe1206974d3dbff0f4e
srfoster <lindseyhandley@gmail.com>
2020-08-19 16:59:16 -0700

    lindsey blog post not finished needs pictures

---
+++ pages/blog/post-2020-08-20.rkt
@@ -0,0 +1,47 @@
+#lang at-exp racket
+
+(require "./lang.rkt")
+
+(define-post 
+  2020-08-20
+  "Lindsey's Hello World"
+  @div{
+    For the past few days, I've been setting up the CodeSpells community infrastructure behind the scenes! Now, it's time to introduce myself!
+  }
+  @div{
+    @p{I met Stephen in July 2008, about a year after he had built his original prototype that would one day evolve into CodeSpells. I remember checking out his Facebook soon after meeting him and being shocked by screenshots of what the community has affectionately termed "The Statue" (see The CodeSpells Story blog post @(a href: "https://codespells.org/posts/the-codespells-story-with-pictures.html" "here")). (I, as a good Southern girl, found The Statue in poor taste.) So I asked him about The Statue, and he told me about this awesome project he had been working on with his friends at college. In those early days, I was a social butterfly who made friends with everyone, but especially cool people who were working on fascinating projects. Stephen and I hit it off! }
+
+    @p{Fast forward to 2014: Stephen and I are attending different PhD programs at UC San Diego, where the CodeSpells research prototype had been conceived and tested in his graduate lab. In our spare time, we began dreaming of what CodeSpells could be in the future and formulated a plan. As Stephen worked with Adrian and Jason to put together the Kickstarter Prototype of CodeSpells, we also worked on prepping for the Kickstarter itself. This is where I truly got involved with the project! My job would be trying to get the backers we needed to make the CodeSpells dream a reality.}
+
+    @p{I was highly involved throughout the Kickstarter, sending out press releases to websites like Gamasutra and Rock, Paper, Shotgun, trying to make GIFs go viral on 9Gag and Imgur, answering backer questions, posting updates on Kickstarter, managing the social media accounts, and just generally sharing the Kickstarter with anyone and everyone who might be interested in an awesome game that would let you code magic spells.}
+
+    @p{The Kickstarter was a wild, exhilarating ride and ended up being a huge success! I tracked metrics throughout the process to keep everyone's spirits high:}
+
+    @(image-grid
+       ; INSERT IMAGES
+       )
+
+    @p{As the Kickstarter's funding campaign came to a close, I continued to promote the game via press releases and social media. I also helped the team share Kickstarter updates and distribute Kickstarter rewards.}
+
+    @p{After the Kickstarter funding phase ended, I worked on CodeSpells on a very part-time basis (we wanted to use MOST of the funds on development and not marketing at this point). So my attention re-focused to putting the finishing touches on my dissertation at UCSD and developing a for-profit computer science education business that would become my full-time job after I finished my PhD program. Later, as development on the Kickstarter version of CodeSpells came to a close, my attention was exclusively focused on starting new coding after-school programs and summer camps for local kids in our local area, with a side goal of trying to get more girls involved in computer science. We were also in the midst of promoting a new educational software for teaching coding to kids through Minecraft called @(a href: "https://www.learntomod.com" "LearnToMod")!}
+
+    @p{My work with ThoughtSTEM was full of excitement, but nothing quite lived up to the thrill of the CodeSpells Kickstarter, nor the sense of community I felt with the wizards who supported the game development throughout the years. When COVID-19 hit, we saw renewed chatter on the community Discord channel. An idea crossed our minds... Stephen's blog post @(a href: "https://codespells.org/posts/the-codespells-story-part-2.html" "here") picks up that story.}
+
+    @p{I'm excited to be back to serve the CodeSpells community! Over the next several months you'll find me:
+      @li{Promoting the Patreon and trying to grow the community}
+      @li{Creating screen recordings of the CodeSpells prototype throughout development}
+      @li{Being a liason between the Community and Stephen (our lead dev), so that he can focus on development}
+      @li{Trying to hype up ice magic as much as possible without pissing off Stephen}
+      @li{Interviewing community members about their experiences with CodeSpells and creating blog posts featuring them}
+      @li{Posting on Social Media}
+      @li{Writing and distributing press releases}
+      @li{Keeping a pulse on the CodeSpells community and doing whatever I need to do to make sure everybody is getting their needs met}
+    }
+
+    @p{I'm SO psyched to see what we can build together, y'all!}
+
+    @p{Stay tuned for more!!}
+
+    @p{- Lindsey D. Handley}
+    @(logo 200)
+  })
