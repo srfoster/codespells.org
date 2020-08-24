b84a66967e4f8a30c0d1a02c9ab15ad140cbc1fb
srfoster <lindseyhandley@gmail.com>
2020-08-24 14:39:44 -0700

    finished hello world lindsey post

--- pages/blog/post-2020-08-20.rkt
+++ pages/blog/post-2020-08-20.rkt
@@ -1,64 +1,5 @@
 #lang at-exp racket
 
-<<<<<<< HEAD
-(require "./lang.rkt")
-
-(define-post 
-  2020-08-20
-  "Lindsey's Hello World"
-  @div{
-    For the past few days, I've been setting up the CodeSpells community infrastructure behind the scenes! Now, it's time to introduce myself!
-  }
-  @div{
-    @p{I met Stephen in July 2008, about a year after he had built his original prototype that would one day evolve into CodeSpells. I remember checking out his Facebook soon after meeting him and being shocked by screenshots of what the community has affectionately termed "The Statue" (see The CodeSpells Story blog post @(a href: "https://codespells.org/posts/the-codespells-story-with-pictures.html" "here")). (I, a good Southern girl, found The Statue in poor taste.) So I asked him about The Statue, and he told me about this awesome project he had been working on with his friends at college. In those early days, I was a social butterfly who made friends with everyone, but especially cool people who were working on fascinating projects. Stephen and I hit it off! }
-
-    @p{Fast forward to 2014: Stephen and I are attending different PhD programs at UC San Diego, where the CodeSpells research prototype had been conceived and tested in his graduate lab. In our spare time, we began dreaming of what CodeSpells could be in the future and formulated a plan. As Stephen worked with Adrian and Jason to put together the Kickstarter Prototype of CodeSpells, we also worked on prepping for the Kickstarter itself. This is where I truly got involved with the project! My job would be trying to get the backers we needed to make the CodeSpells dream a reality.}
-
-    @p{I was highly involved throughout the Kickstarter, sending out press releases to websites like @(a href: "https://www.gamasutra.com/blogs/LindseyHandley/20140918/225984/Kickstarting_a_Coding_Education_Video_Game_in_a_PostquotCode_Heroquot_World.php" "Gamasutra"), @(a href: "https://www.cnet.com/news/codespells-write-code-invent-magic-spells/" "CNET"), and @(a href: "https://www.rockpapershotgun.com/2014/09/02/codespells-proof-that-coding-is-arcane-wizardry/" "Rock, Paper, Shotgun"), trying to make GIFs go viral on 9Gag and @(a href: "https://imgur.com/gallery/Uvgt5" "Imgur"), answering backer questions, posting updates on Kickstarter, managing the social media accounts, and just generally sharing the Kickstarter with anyone and everyone who might be interested in an awesome game that would let you code magic spells. The Kickstarter was a wild, exhilarating ride and ended up being a huge success!}
-
-    @p{As the Kickstarter's funding campaign came to a close, I continued to promote the game via press releases and social media. I also helped the team share Kickstarter updates and distribute Kickstarter rewards.}
-
-    @p{After the Kickstarter funding phase ended, I worked on CodeSpells on a very part-time basis (we wanted to use MOST of the funds on development and not marketing at this point). So my attention re-focused on putting the finishing touches on my dissertation at UCSD and developing a for-profit computer science education business that would become my full-time job after I finished my PhD program. Later, as development on the Kickstarter version of CodeSpells came to a close, my passion for computer science education grew. For several years, my work with ThoughtSTEM centered around starting new coding after-school programs and summer camps for local kiddos, with a secondary goal of trying to get more girls involved in computer science. I also helped promote a new educational software we built for teaching coding to kids through Minecraft called @(a href: "https://www.learntomod.com" "LearnToMod")! Within the last year we started a new nonprofit called MetaCoders that's currently teaching @(a href: "https://metacoders.org/coding-club.html" "online coding classes")  to kids via Zoom during COVID-19, and wrote a book about teaching and learning computer science called @(a href: "https://amzn.to/3aIBflN" @emph(Don't Teach Coding: Until You Read This Book)).}
-
-    @(image-grid
-        (cons img:lindsey-with-kid.jpg 
-              @p{
-              Teaching kids coding at a local STEM fair.})
-        (cons img:lindsey-at-minecon.jpg 
-              @p{
-              Hanging out with an Enderdragon at Minecon while promoting LearnToMod.})
-        (cons img:lindsey-stephen-professional.jpg 
-              @p{
-              Lindsey & Stephen, trying to be all professional for once.})
-        (cons img:lindsey-at-workshop.jpg 
-              @p{
-              Working with some of our students in a coding workshop.})
-        
-        )
-    
-    @p{My work with ThoughtSTEM was full of excitement, but nothing quite lived up to the thrill of the CodeSpells Kickstarter, nor the sense of community I felt with the wizards who supported the game's development throughout the years. When COVID-19 hit, we noticed renewed conversation on the community Discord channel, and an idea crossed our minds... Stephen's blog post @(a href: "https://codespells.org/posts/the-codespells-story-part-2.html" "here") picks up that story.}
-
-    @p{I'm excited to be back to serve the CodeSpells community! Over the next several months you'll find me:
-      @li{Promoting the Patreon and trying to grow the community}
-      @li{Creating screen recordings of the CodeSpells prototype throughout development}
-      @li{Trying to get computer science educators involved in the CodeSpells community}
-      @li{Being a liason between the Community and Stephen (our lead dev), so that he can focus on development}
-      @li{Trying to hype up ice magic as much as possible without pissing off Stephen}
-      @li{Interviewing community members about their experiences with CodeSpells and creating blog posts featuring them - DM me if you're interested!}
-      @li{Posting on Social Media}
-      @li{Writing and distributing press releases when the time comes}
-      @li{Helping Stephen a bit on development with the semi-limited coding & art skills I do have}
-      @li{Keeping a pulse on the CodeSpells community and doing whatever I need to do to make sure everybody is getting their needs met}
-    }
-
-    @p{I'm SO psyched to see what we can build together, y'all!}
-
-    @p{Stay tuned for more!!}
-
-    @p{- Lindsey D. Handley}
-    @(logo 200)
-  })
-=======
 
 ;TODO: make this a legit package.
 (require "../../lang.rkt")
@@ -492,5 +433,3 @@
        })
 
 
-
->>>>>>> acde2ac1c9cf1da7381360bb0d8527605f054671
