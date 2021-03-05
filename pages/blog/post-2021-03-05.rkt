c88100480d751d8e7ddb072664395134da7e46f0
Stephen Foster <stephen@thoughtstem.com>
2021-03-05 13:30:19 -0800

    new post

---
+++ pages/blog/post-2021-03-05.rkt
@@ -0,0 +1,55 @@
+
+#lang at-exp racket
+
+(require "../../lang.rkt")
+(require "./lang.rkt")
+
+(define-post
+  2021-03-05
+  "CodeSpells Season 1. Nomic update"
+  @div{
+    Lindsey seems to be winning
+  }
+  @md{
+
+  It's hard to predict who will win a game that is being developed while we're playing it, but if we had to guess, Lindsey seems to have a slight edge.
+
+  Lindsey has chosen to adopt a creature-focused strategy, whereas Stephen's strategy is more focused on control.
+
+  Lindsey summoned a Sevarog and began advancing it toward Stephen's nexus.  Stephen's attempt to land a Parasite on Lindsey's Sevarog was successful -- but Lindsey managed to remove it with a Disenchant Spell.
+
+  The current state of the game:
+
+  * Stephen:
+      - 1 Nexus (100 Mana)
+      - 1 Pylon (+10 Mana per turn)
+
+  * Lindsey:
+      - 1 Nexus (0 Mana)
+      - 1 Sevarog (100 Mana)
+
+  Lindsey has more potential to do damage, but Stephen seems to
+  have a structural advantage.
+
+  ---
+
+  In other news, we've been working on a 
+  Twitch bot, which lets you both add songs
+  to a playlist that plays during our stream,
+  and more importantly, lets you affect the
+  state of the 3D world simply by typing 
+  commands in the Twitch chat.
+
+  We'll be using this to give Twitch viewers
+  more ways of interacting with our 
+  design stream over the coming weeks!
+
+  ---
+
+  A big thanks to everyone who has been
+  showing up day after day to help us with
+  this project! 
+
+  @p{- Stephen R. Foster}
+  @(logo 200)
+  })
