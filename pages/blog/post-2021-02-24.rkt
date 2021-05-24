9151e2dcb1d605931dec337460448677c155074a
Jess Cummins <me@jessecummins.com>
2021-05-24 01:38:38 -0400

    standardize formatting of blog entry (2021-02-24) and add details about Twitch streaming schedule

--- pages/blog/post-2021-02-24.rkt
+++ pages/blog/post-2021-02-24.rkt
@@ -10,24 +10,24 @@
   The competition is on. Whoever loses must wear the Loser's Cloak in public.
   }
   @md{
-  Now that we've worked out a lot of the initial kinks with multiplayer, we're finally at a place where we can start designing an actual multiplayer game experience! Stephen and I are going to start designing the CodeSpells Wizard Tournament experience through a game of [Nomic](https://en.wikipedia.org/wiki/Nomic). 
-  
-  We will play 3 games in this Inaugural Season of CodeSpells. Best 2 out of 3 wins. However, unlike future Seasons, each game in this Season will likely span multiple days, as we develop new multiplayer features to assist in our gameplay. You can watch these game sessions live on [Twitch](https://www.twitch.tv/codespells) every morning (Pacific Standard Time), M-F.  
+  Now that we've worked out a lot of the initial kinks with multiplayer, we're finally at a place where we can start designing an actual multiplayer game experience! Stephen and I are going to start designing the CodeSpells Wizard Tournament experience through a game of [Nomic](https://en.wikipedia.org/wiki/Nomic).
 
-  These are the stakes for the game:  
+  We will play 3 games in this Inaugural Season of CodeSpells. Best 2 out of 3 wins. However, unlike future Seasons, each game in this Season will likely span multiple days, as we develop new multiplayer features to assist in our gameplay. You can watch these game sessions live on [Twitch](https://www.twitch.tv/codespells) every morning (Pacific Standard Time (UTC-8)), 9:30 AM to 12:30 PM, Monday through Friday.
+
+  These are the stakes for the game:
 
   * Loser must wear a Loser's Cloak on stream until the beginning of the 2nd CodeSpells Season.
-  * The loser will also wear the Loser's Cloak in public once. This will be recorded by the winner. 
+  * The loser will also wear the Loser's Cloak in public once. This will be recorded by the winner.
   * The winner will decide what the Loser's Cloak is.
 
   Preliminary Game Rules:
-  
+
   * New rules can be created by unanimous agreement between Stephen and Lindsey.
   * Old rules can be revised by unanimous agreement.
   * Certain rules can be implemented by code, also by unanimous agreement.
   * This CodeSpells Season will be played in Arena World.
 
-  As the Season progresses and we add more rules, we'll add them to blog posts. We hope you have a chance to pop into one of our live Twitch game sessions to say "Hello" or help contribute ideas for gameplay! We have really been enjoying getting live feedback from our [Twitch](https://www.twitch.tv/codespells) audience. 
+  As the Season progresses and we add more rules, we'll add them to blog posts. We hope you have a chance to pop into one of our live Twitch game sessions to say "Hello" or help contribute ideas for gameplay! We have really been enjoying getting live feedback from our [Twitch](https://www.twitch.tv/codespells) audience.
 
   @p{- Lindsey D. Handley}
   @(logo 200)
